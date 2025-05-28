0fed5e8f858093305c7b25a3cf68479f226fe6d8
mtruong727 <mtruong727@outlook.com>
2025-05-28 13:57:46 -0500

    Add hero list

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <iomanip>
+#include <vector>
 #include <map>
 
 int main() 
 {
-    std::map<std::string, int> heroList{{},{}};
+    std::map<int, std::string> heroList{ {001, "Warrior"},{002, "Archer"}, {003, "Mage"}, {004, "Healer"}};
+
+    std::cout << "The size of hero list is: " << heroList.size() << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << heroList[001] << std::endl;
+    std::cout << heroList[002] << std::endl;
+    std::cout << heroList[003] << std::endl;
+    std::cout << heroList[004] << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Replace Warrior with Tank..." << std::endl;
+    
+    std::cout << std::endl;
+
+    std::cout << "The size of hero list is: " << heroList.size() << std::endl;
+
+    std::cout << std::endl;
+    
+    heroList[001] = "Tank";
+    std::cout << heroList[001] << std::endl;
+    std::cout << heroList[002] << std::endl;
+    std::cout << heroList[003] << std::endl;
+    std::cout << heroList[004] << std::endl;
+
+    std::cout << std::endl;
 
 
     return 0;
